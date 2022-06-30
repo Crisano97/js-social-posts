5177d268ae0dd16a17600a8461ff8b6d3bb0ce5d
Crisano97 <97keb6@gmail.com>
2022-06-30 17:43:07 +0200

    Milestone 2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "is_liked" : true,
+        "is_liked": true,
         "created": "2022-06-25"
     },
     {
@@ -20,7 +20,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=10"
         },
         "likes": 120,
-        "is_liked" : false,
+        "is_liked": false,
         "created": "2022-06-03"
     },
     {
@@ -32,7 +32,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 78,
-        "is_liked" : true,
+        "is_liked": true,
         "created": "2022-05-15"
     },
     {
@@ -44,7 +44,7 @@ const posts = [
             "image": null
         },
         "likes": 56,
-        "is_liked" : false,
+        "is_liked": false,
         "created": "2022-04-03"
     },
     {
@@ -56,7 +56,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-         "is_liked" : false,
+        "is_liked": false,
         "created": "2022-03-05"
     },
     {
@@ -68,7 +68,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=33"
         },
         "likes": 95,
-         "is_liked" : true,
+        "is_liked": true,
         "created": "2022-02-02"
     },
     {
@@ -80,7 +80,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=59"
         },
         "likes": 95,
-         "is_liked" : false,
+        "is_liked": false,
         "created": "2022-02-01"
     },
     {
@@ -92,88 +92,89 @@ const posts = [
             "image": "null"
         },
         "likes": 95,
-         "is_liked" : true,
+        "is_liked": true,
         "created": "2021-12-11"
     }
 ];
 
-posts.forEach((element) =>{
+posts.forEach((element) => {
 
-const postListContainer = document.getElementById('container');
+    const postListContainer = document.getElementById('container');
 
-const newPostFullContainer = createNewElement('div', 'post');
+    const newPostFullContainer = createNewElement('div', 'post');
 
-const postHeader = createNewElement('div', 'post__header');
+    const postHeader = createNewElement('div', 'post__header');
 
-const postMeta = createNewElement('div', 'post-meta');
+    const postMeta = createNewElement('div', 'post-meta');
 
-const postMetaIcon = createNewElement('div', 'post-meta__icon');
-const profilePic = createNewImg(element.author.image, element.author.name, 'profile-pic')
+    const postMetaIcon = createNewElement('div', 'post-meta__icon');
+    const profilePic = createNewImg(element.author.image, element.author.name, 'profile-pic')
 
-const postMetaData = createNewElement('div', 'post-meta__data');
-const postMetaAuthor =createNewElement('div', 'post-meta__author');
-postMetaAuthor.innerHTML = element.author.name;
-const postMetaTime = createNewElement('div', 'post-meta__time');
-postMetaTime.innerHTML = element.created;
+    const postMetaData = createNewElement('div', 'post-meta__data');
+    const postMetaAuthor = createNewElement('div', 'post-meta__author');
+    postMetaAuthor.innerHTML = element.author.name;
+    const postMetaTime = createNewElement('div', 'post-meta__time');
+    postMetaTime.innerHTML = element.created;
 
-const postText = createNewElement('div', 'post__text');
-postText.innerHTML = element.content;
+    const postText = createNewElement('div', 'post__text');
+    postText.innerHTML = element.content;
 
-const postImageContainer = createNewElement('div', 'post__image');
-const postImage = createNewImg(element.media);
+    const postImageContainer = createNewElement('div', 'post__image');
+    const postImage = createNewImg(element.media);
 
-const postFooter = createNewElement('div', 'post__footer');
+    const postFooter = createNewElement('div', 'post__footer');
 
-const postLikes = createNewElementWithTwoClass('div', 'likes', 'js-likes');
+    const postLikes = createNewElementWithTwoClass('div', 'likes', 'js-likes');
 
-const buttonContainer = createNewElement('div', 'likes__cta');
-const button = createNewElementWithTwoClass('a', 'like-button', 'js-like-button');
-button.setAttribute('href', '#');
-button.setAttribute('data-postid', element.id);
+    const buttonContainer = createNewElement('div', 'likes__cta');
+    const button = createNewElementWithTwoClass('a', 'like-button', 'js-like-button');
+    button.setAttribute('href', '#');
+    button.setAttribute('data-postid', element.id);
 
-const iconLike = createNewElementWithTwoClass('like-button__icon', 'fas');
-iconLike.classList.add('fa-thumbs-up')
-const likeText = createNewElement('span', 'like-button__label');
-likeText.innerHTML = "Mi Piace";
+    const iconLike = createNewElementWithTwoClass('like-button__icon', 'fas');
+    iconLike.classList.add('fa-thumbs-up')
+    const likeText = createNewElement('span', 'like-button__label');
+    likeText.innerHTML = "Mi Piace";
 
-const likesCounter = createNewElement('div', 'likes__counter');
-likesCounter.innerHTML = `piace a <b>${element.likes}</b> persone  `;
-console.log(likesCounter.innerHTML)
+    const likesCounter = createNewElement('div', 'likes__counter');
+    likesCounter.innerHTML = `piace a <b>${element.likes}</b> persone  `;
 
+    button.addEventListener('click', function () {
+        button.classList.add('like-button--liked');
+    })
 
-
-
-
-postListContainer.append(newPostFullContainer);
-newPostFullContainer.append(postHeader, postText, postImageContainer, postFooter);
-postHeader.append(postMeta);
-postMeta.append(postMetaIcon, postMetaData);
-postMetaIcon.append(profilePic);
-postMetaData.append(postMetaAuthor, postMetaTime);
-postImageContainer.append(postImage);
-postFooter.append(postLikes);
-postLikes.append(buttonContainer, likesCounter);
-buttonContainer.append(button);
-button.append(iconLike, likeText);
+    postListContainer.append(newPostFullContainer);
+    newPostFullContainer.append(postHeader, postText, postImageContainer, postFooter);
+    postHeader.append(postMeta);
+    postMeta.append(postMetaIcon, postMetaData);
+    postMetaIcon.append(profilePic);
+    postMetaData.append(postMetaAuthor, postMetaTime);
+    postImageContainer.append(postImage);
+    postFooter.append(postLikes);
+    postLikes.append(buttonContainer, likesCounter);
+    buttonContainer.append(button);
+    button.append(iconLike, likeText);
 });
 
-function createNewElement (element, elementClass) {
+function createNewElement(element, elementClass) {
     const newElement = document.createElement(element);
     newElement.classList.add(elementClass);
     return newElement;
 }
 
-function createNewElementWithTwoClass (element, elementClass1, elementClass2) {
+function createNewElementWithTwoClass(element, elementClass1, elementClass2) {
     const newElement = document.createElement(element);
     newElement.classList.add(elementClass1, elementClass2);
     return newElement;
 }
 
-function createNewImg (imageUrl, imageAlt, imageClass) {
+function createNewImg(imageUrl, imageAlt, imageClass) {
+
     const newImg = document.createElement('img');
+
     newImg.classList.add(imageClass);
     newImg.setAttribute('src', imageUrl);
     newImg.setAttribute('alt', imageAlt);
-
     return newImg;
+
 }
