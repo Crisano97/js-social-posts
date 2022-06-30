3c1fe842bcb0e0f706fd750d35f95ecd11ed99cf
Crisano97 <97keb6@gmail.com>
2022-06-30 15:56:08 +0200

    post header

---
+++ main.js
@@ -0,0 +1,138 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "is_liked" : true,
+        "created": "2022-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "is_liked" : false,
+        "created": "2022-06-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "is_liked" : true,
+        "created": "2022-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "is_liked" : false,
+        "created": "2022-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Stefano Tortellini",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+         "is_liked" : false,
+        "created": "2022-03-05"
+    },
+    {
+        "id": 6,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=536",
+        "author": {
+            "name": "Luigia Micca",
+            "image": "https://unsplash.it/300/300?image=33"
+        },
+        "likes": 95,
+         "is_liked" : true,
+        "created": "2022-02-02"
+    },
+    {
+        "id": 7,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=531",
+        "author": {
+            "name": "Grace Hunterdan",
+            "image": "https://unsplash.it/300/300?image=59"
+        },
+        "likes": 95,
+         "is_liked" : false,
+        "created": "2022-02-01"
+    },
+    {
+        "id": 8,
+        "content": "Ao! Che nun ce lo sai che io so l'unico post scritto in romanesco de tutta sta lista autogenerata! Dico e scrivo nummeri da quanno so nato, ce manca pure che me faccio un post autogennerato!",
+        "media": "https://unsplash.it/600/400?image=554",
+        "author": {
+            "name": "Mario Di Nio",
+            "image": "null"
+        },
+        "likes": 95,
+         "is_liked" : true,
+        "created": "2021-12-11"
+    }
+];
+
+posts.forEach((element) =>{
+
+const postListContainer = document.getElementById('container');
+const newPostFullContainer = createNewElement('div', 'post');
+const postHeader = createNewElement('div', 'post__header');
+const postMeta = createNewElement('div', 'post-meta');
+const postMetaIcon = createNewElement('div', 'post-meta__icon');
+const profilePic = createNewImg(element.author.image, element.author.name, 'profile-pic')
+const postMetaData = createNewElement('div', 'post-meta__data');
+const postMetaAuthor =createNewElement('div', 'post-meta__author');
+postMetaAuthor.innerHTML = element.author.name;
+const postMetaTime = createNewElement('div', 'post-meta__time');
+postMetaTime.innerHTML = element.created;
+
+postListContainer.append(newPostFullContainer);
+newPostFullContainer.append(postHeader);
+postHeader.append(postMeta);
+postMeta.append(postMetaIcon, postMetaData);
+postMetaIcon.append(profilePic);
+postMetaData.append(postMetaAuthor, postMetaTime);
+});
+
+
+
+
+function createNewElement (element, elementClass) {
+    const newElement = document.createElement(element);
+    newElement.classList.add(elementClass);
+    return newElement;
+}
+
+function createNewImg (imageUrl, imageAlt, imageClass) {
+    const newImg = document.createElement('img');
+    newImg.classList.add(imageClass);
+    newImg.setAttribute('src', imageUrl);
+    newImg.setAttribute('alt', imageAlt);
+
+    return newImg;
+}
